9a092e47d321a5595fb3944008a5cf7281572887
Swetha Ramaswamy <swetharamaswamy66@gmail.com>
2018-04-05 22:38:32 -0500

    stopword removal done

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,10 +4,17 @@ import string
 import re
 import nltk
 from nltk.corpus import stopwords
-#things to do in this program: 1) generate tokens new line is a new tweet hence one token at a time, give it to remove stopwords
-#just remove the hashtag , compare with the two dictionaries and save the output in a new file maybe?
-abbr = open("abbreviations_dictionary.txt","r", encoding="utf-8")
-abbrl=abbr.readlines()
+#
+alist=[]
+with open('engdict.txt') as f:
+    alist = [line.rstrip() for line in f]
+
+abbrlist=[]
+with open('abbreviations_dictionary.txt') as f:
+    abbrlist = [line.rstrip() for line in f]
+# print(abbrlist)
+# things to do in this program: 1) generate tokens new line is a new tweet hence one token at a time, give it to remove stopwords
+# just remove the hashtag , compare with the two dictionaries and save the output in a new file maybe?
 edited_words=[]
 def is_alpha(word):
     try:
@@ -32,18 +39,27 @@ def removestuff(tokens):
                             continue
                         else:
                             edited_words.append(token)
-        # abbreviation(edited_words)
+                            # checkdict(token)
+
+def abbrcheck(word):
+    for i in abbrlist:
+        if re.match(r'\b' + word + r'\b', i):
+            print(i)
+
+# def wordInList(word, listOfWords):
+#     for i in listOfWords:
+#         if re.match(r'\b' + word + r'\b', i):
+
+def checkdict(editedwords):
+    for word in edited_words:
+        if word not in alist:
+            abbrcheck(word)
 
 
-# def abbreviation(words):
-#     for word in words:
-
-for i in range(1,6):
-    f = open("input" + str(i)+".txt", "r", encoding="utf-8")
-    lines = f.readlines()
-    for line in lines:
-        normalize(line)
-
-print(edited_words)
-
-
+# for i in range(1,6):
+#     # f = open("input" + str(i)+".txt", "r", encoding="utf-8")
+#     # lines = f.readlines()
+#     # for line in lines:
+normalize("RT lol brb #datascience this is http:""/.893849  data that I am talking about. I love data man! #love acha pasand hai mujhe data")
+checkdict(edited_words)
+# print(edited_words)
--- getinput.py
+++ getinput.py
@@ -1,4 +1,6 @@
 import tweepy
+import nltk
+from nltk.corpus import stopwords
 consumer_key= 'Quo2d5FNN6F9LNwqOExTufMxR'
 consumer_secret='qKHT8wy8PGrf7NcsONuUtk0RWhnvCrYbNp6NPqFOJbR4ZQldsT'
 
@@ -10,22 +12,65 @@ auth.set_access_token(access_token,access_token_secret)
 
 
 api=tweepy.API(auth)
-# f=open("input1","w",encoding="utf-8")
+train=open("train","w",encoding="utf-8")
+test=open("test","w",encoding="utf-8")
+#experimental section using lists
 i=1
-hashtags=['data',"datascience","animalrescue","peace","marchforourlives"]
-for tag in hashtags:
-        k=1
-        f = open("input" + str(i)+".txt", "w", encoding="utf-8")
-        f1 =open("input" + str(i)+"test.txt", "w", encoding="utf-8")
-        for tweet in tweepy.Cursor(api.search,q='#'+tag).items(50):
-            if k<36:
-                f.write(str(k)+":"+str(tweet.text))
-                print(str(k)+":"+tweet.text)
-                k+=1
-            else:
-                f1.write(str(k) + ":" + str(tweet.text))
-                print(str(k) + ":" + tweet.text)
-                k += 1
 
-        i+=1
-print("Input done for all hashtags ")
+
+
+########################################################
+k=1
+hashtags=['data',"datascience","machinelearning","bigdata","analytics","python","deeplearning","datascientists","AI","ML"]
+list=[]
+listtest=[]
+editedtest=[]
+editedtrain=[]
+for tag in hashtags:
+    for tweet in tweepy.Cursor(api.search,q='#'+tag).items(50):
+                if k<36:
+                    # f.write(str(k)+":"+str(tweet.text))
+                    list.append(tweet.text)
+                    # print(str(k)+":"+tweet.text)
+                    k+=1
+                else:
+                    # f1.write(str(k) + ":" + str(tweet.text))
+                    listtest.append(tweet.text)
+                    # print(str(k) + ":" + tweet.text)
+                    k+= 1
+
+
+def removestuff(tokens):
+        for token in tokens:
+            if token.isalnum():
+                if token.isnumeric():
+                    continue
+                else:
+                    if token in ["RT", "https", "http"]:
+                        continue
+                    else:
+                        if token in stopwords.words('english'):
+                            continue
+                        else:
+                            # if key=="set":
+                            editedtrain.append(token)
+                            # else:
+                            #     editedtest.append(token)
+                            # print(token)
+
+
+def normalize(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    removestuff(tokens)
+key="set"
+for tweet in list:
+    normalize(tweet)
+
+# key="unset"
+# for tweet in listtest:
+#     normalize(tweet)
+
+# print("train set")
+# print(editedtrain)
+# print("test set")
+# print(editedtest)
